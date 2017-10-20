b2430e4a666f6507e01189868acefc2ed64001bf
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-10-20 09:47:49 -0400

    ipv4.py - more modifications

--- base/_modules/ipv4.py
+++ base/_modules/ipv4.py
@@ -1,2 +1,12 @@
-def get_ipv4():
-    return __grains__['ip4_interfaces'](eth0)
+'''
+This module will be used to return the IPv4 information, which can then be evaulated by a state file
+'''
+__virtualname__ = 'ipv4'
+
+import salt
+
+def get_eth0():
+	'''
+	Returns the IPv4 IP address
+	'''
+    return __grains__['ip4_interfaces']
