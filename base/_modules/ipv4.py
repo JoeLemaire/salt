cce35b39f384d822766664498cc70ae30a2580d6
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-10-20 11:29:08 -0400

    ipv4.py - cleanup

--- base/_modules/ipv4.py
+++ base/_modules/ipv4.py
@@ -1,18 +1,8 @@
 
-# _modules/ipv4.py
-#!python
-
 '''
 This module will be used to return the IPv4 information, which can then be evaulated by a state file
 '''
-'''
-__virtualname__ = 'ipv4'
 
-def __virtual__():
-	if __grains__['os_family'] == 'RedHat':
-		return __virtualname__
-	return False
-'''
 import salt
 
 def get_eth0():
