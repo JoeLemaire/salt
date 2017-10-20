f396f744cbec9fcd408aaf1d00563116ac887c87
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-10-20 10:08:26 -0400

    ipv4.py - more modifications

--- base/_modules/ipv4.py
+++ base/_modules/ipv4.py
@@ -1,16 +1,15 @@
+
 '''
 This module will be used to return the IPv4 information, which can then be evaulated by a state file
+'''
 '''
 __virtualname__ = 'ipv4'
 
 def __virtual__():
-	'''
-	This module can only be called by a RedHat variant
-	'''
 	if __grains__['os_family'] == 'RedHat':
 		return __virtualname__
 	return False
-
+'''
 import salt
 
 def get_eth0():
