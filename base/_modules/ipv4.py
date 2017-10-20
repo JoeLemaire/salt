5db049f1e27b1f1a79d254d831843c727af35702
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-10-20 11:45:47 -0400

    ipv4.py - return id

--- base/_modules/ipv4.py
+++ base/_modules/ipv4.py
@@ -6,7 +6,7 @@ This module will be used to return the IPv4 information, which can then be evaul
 import salt
 
 def get_eth0():
-	'''
-	Returns the IPv4 IP address
-	'''
+    '''
+    Returns the IPv4 IP address
+    '''
     return __grains__['id']
