80022ceb04ed299cae3c068dbf34b602c89e0b28
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-11-13 10:42:02 -0500

    fixing proxy_type grain

--- base/_grains/proxy_type.py
+++ base/_grains/proxy_type.py
@@ -15,7 +15,7 @@ def proxy_type():
     grains = {}
 
     #if (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'haproxy' and __salt__['pkg.version'] != 'httpd'):.
-    if (__grains__['os'] == 'CentOS'):
+    if __grains__['os'] == 'CentOS':
         grains['proxy_type'] = 'haproxy'
     '''
     elif (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'httpd' and __salt__['pkg.version'] != 'haproxy'):
