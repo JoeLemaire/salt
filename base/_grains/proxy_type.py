c2aa1548c85a6bbf59077e628cebb4ac428cd603
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-11-13 10:40:04 -0500

    fixing proxy_type grain

--- base/_grains/proxy_type.py
+++ base/_grains/proxy_type.py
@@ -14,7 +14,8 @@ import salt.modules.yumpkg
 def proxy_type():
     grains = {}
 
-    if (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'haproxy' and __salt__['pkg.version'] != 'httpd'):
+    #if (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'haproxy' and __salt__['pkg.version'] != 'httpd'):.
+    if (__grains__['os'] == 'CentOS'):
         grains['proxy_type'] = 'haproxy'
     '''
     elif (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'httpd' and __salt__['pkg.version'] != 'haproxy'):
