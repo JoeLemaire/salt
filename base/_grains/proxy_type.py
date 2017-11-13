a12920b31019ce0d9364506e105434a037dabb8b
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-11-13 10:22:18 -0500

    fixing proxy_type grain

--- base/_grains/proxy_type.py
+++ base/_grains/proxy_type.py
@@ -1,24 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 '''
 File: # base\_grains\rp_type.py
 Description: This determines whether a reverse proxy is HAProxy-based or Apache-based 
 '''
-
+'''
 from __future__ import absolute_import
 import logging
 import salt
 import os
 import salt.modules.yumpkg
-
+'''
 
 def proxy_type():
-#    if (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'haproxy' and __salt__['pkg.version'] != 'httpd'):
-    if (__grains__['os'] == 'CentOS'):
+'''
+    if (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'haproxy' and __salt__['pkg.version'] != 'httpd'):
         return {'proxy_type':'haproxy'}
-#    elif (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'httpd' and __salt__['pkg.version'] != 'haproxy'):
-#        return {'proxy_type':'apache'}
-#    else:
-#        return {'proxy_type:'natta'} 
-
-if __name__ == "__main__":
-    print proxy_type()
+    elif (__grains__['os'] == 'CentOS' and __salt__['pkg.version'] == 'httpd' and __salt__['pkg.version'] != 'haproxy'):
+        return {'proxy_type':'apache'}
+    else:
+        return {'proxy_type:'natta'}
+'''
+    grains = {}
+    grains['testgrain'] = 'works'
+    return grains
