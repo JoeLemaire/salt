4748a136d178f1c77443b00b2adb304141d709b6
Joe Lemaire <joe.lemaire@vtinfo.com>
2017-11-13 10:47:08 -0500

    fixing proxy_type grain

--- base/_grains/proxy_type.py
+++ base/_grains/proxy_type.py
@@ -3,13 +3,13 @@
 File: # base\_grains\rp_type.py
 Description: This determines whether a reverse proxy is HAProxy-based or Apache-based 
 '''
-'''
+
 from __future__ import absolute_import
 import logging
 import salt
 import os
 import salt.modules.yumpkg
-'''
+
 
 def proxy_type():
     grains = {}
